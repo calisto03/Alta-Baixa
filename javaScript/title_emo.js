e04d9f4160523ab70efd54130f858a9c4bb241bc
FranciscaC <fcalisto@student.uc.pt>
2022-01-16 19:15:27 +0000

    Transição bem sucedida

--- javaScript/title_emo.js
+++ javaScript/title_emo.js
@@ -5,32 +5,22 @@ var element_emo_4 =document.getElementById("intro_emo_4");
 var element_emo_5 =document.getElementById("intro_emo_5");
 var element_emo_6 =document.getElementById("intro_emo_6");
 var elemento_about=document.getElementById("about");
+var moveFlag = 0;
 
-/*function move(){
-    if(elemento_about.style.right==0){
-        elemento_about.addEventListener("click", function() {
-        elemento_about.style.right = -55+"vh";
-    })} else
-        if(elemento_about.style.right==-55+"vh"){
-        elemento_about.addEventListener("click", function() {
-            elemento_about.style.right = 0;
-    })}
-
-}
-move();*/
-
+elemento_about.addEventListener("click", move);
 
 function move(){
-    if(elemento_about.style.transform="translateX(0)"){
-        elemento_about.addEventListener("click", function() {
-            elemento_about.style.transform ="translateX(55vh)";
-        })} else
-    if(elemento_about.style.transform="translateX(55vh)"){
-        elemento_about.addEventListener("click", function() {
-            elemento_about.style.transform="translateX(0)";
-        })}
+
+    if(moveFlag == 0){
+        elemento_about.style.transform ="translateX(0)";
+        moveFlag = 1;
+    }
+    else if(moveFlag == 1){
+        elemento_about.style.transform="translateX(57vh)";
+        moveFlag = 0;
+    }
 }
-move();
+
 
 
 
