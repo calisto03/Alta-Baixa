1e4cbe755bf1cc46f7341a5c65234a91b7ff1814
Laura Pais <pais.laura@gmail.com>
2022-01-17 22:57:09 +0000

    +

--- javaScript/random_img.js
+++ javaScript/random_img.js
@@ -2,17 +2,28 @@ function getRandomInt(max) {
     return Math.floor(Math.random() * max)
 }
 
-var imageArray = ["1.png", "2.png"];
+//var artigo_1=document.getElementById("artigo_1");
 
-var image = document.getElementsByClassName("image_index");
+//var imageArray = ["1.png", "2.png"];
 
-for(var i=0; i<image.length;i++) {
-    image[i].src = "data/img/index/" + imageArray[getRandomInt(imageArray.length)];
+for(let j=0; j<artigos.length; j++) {
+    load_index_img(artigos[j]);
+}
 
-    var vh = window.innerHeight, vw = window.innerWidth, img_width = image[i].width, img_height = image[i].height;
-    var newTop = img_height/2 + getRandomInt(vh - img_height);
-    var newLeft = img_width/2 + getRandomInt(vw - img_width);
 
-    image[i].style.top = newTop + "px";
-    image[i].style.left = newLeft + "px";
+function load_index_img(artigo) {
+    var image = artigo.getElementsByClassName("image_artigo");
+
+    for (var i = 0; i < image.length; i++) {
+        ///image[i].src = "data/img/index/1/" + imageArray[getRandomInt(imageArray.length)];
+
+        var vh = artigo.clientHeight, vw = artigo.clientWidth, img_width = image[i].width,
+            img_height = image[i].height;
+
+        var newTop = img_height / 2 + getRandomInt(vh - img_height);
+        var newLeft = img_width / 2 + getRandomInt(vw - img_width);
+
+        image[i].style.top = newTop + "px";
+        image[i].style.left = newLeft + "px";
+    }
 }
